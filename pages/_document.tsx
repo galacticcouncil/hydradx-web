9a57c993b3947c7fd5d8f44480c554bb2b4a2fed
Darken141 <Darken141@gmail.com>
2022-12-02 19:40:45 +0100

    import fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,6 +37,17 @@ export default class MyDocument extends Document {
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<meta name="theme-color" content="#000" />
 					<link rel="icon" href="/favicon.ico" />
+
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Chakra+Petch&family=Poppins:wght@400;500&display=swap"
+						rel="stylesheet"
+					/>
 				</Head>
 				<body>
 					<Main />
